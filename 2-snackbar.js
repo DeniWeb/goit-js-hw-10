d40f0a7087034d90ba9a5fe0a4fd0ffa1cf6a70b
DeniWeb <98588545+DeniWeb@users.noreply.github.com>
2025-02-06 21:51:07 +0000

    Deploying to gh-pages from @ DeniWeb/goit-js-hw-10@fddf717e8b77cfb58eaeac7bc794e6f73e9c3708 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import"./assets/vendor-BbbuE1sJ.js";const t={form:document.querySelector(".form"),delay:document.querySelector(".delay"),state:document.querySelector(".state"),button:document.querySelector("button")};t.form.addEventListener("submit",e=>{e.preventDefault()});
+//# sourceMappingURL=2-snackbar.js.map
